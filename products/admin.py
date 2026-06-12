48a3d805230da28b9f3860f9e0518dd067def636
Michael Provost <mprovost129@gmail.com>
2026-06-12 14:33:23 -0400

    Add product duplication feature in admin panel with corresponding template

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,7 @@ import csv
 
 from django.contrib import admin, messages
 from django.db.models import Count
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import path, reverse
 from django.utils import timezone
 from django.utils.html import format_html
@@ -27,6 +27,7 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     change_list_template = 'admin/products/product/change_list.html'
+    change_form_template = 'admin/products/product/change_form.html'
     list_display = (
         'name', 'get_categories', 'review_status', 'is_active', 'is_featured',
         'order', 'click_count', 'last_checked_at', 'next_review_at', 'created_at',
@@ -43,7 +44,7 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     readonly_fields = ('created_at',)
     filter_horizontal = ('categories', 'tags', 'related_products')
-    actions = ('mark_current', 'mark_needs_review', 'mark_inactive', 'export_selected_csv')
+    actions = ('mark_current', 'mark_needs_review', 'mark_inactive', 'export_selected_csv', 'duplicate_products')
     fieldsets = (
         (None, {
             'fields': ('categories', 'tags', 'related_products', 'name', 'slug', 'description', 'amazon_url', 'amazon_asin', 'image_url'),
@@ -72,8 +73,35 @@ class ProductAdmin(admin.ModelAdmin):
         urls = super().get_urls()
         custom_urls = [
             path('export-template/', self.admin_site.admin_view(self.export_template), name='products_product_export_template'),
+            path('<int:pk>/duplicate/', self.admin_site.admin_view(self.duplicate_view), name='products_product_duplicate'),
         ]
         return custom_urls + urls
+
+    def duplicate_view(self, request, pk):
+        original = self.get_object(request, pk)
+        if original is None:
+            self.message_user(request, 'Product not found.', messages.ERROR)
+            return HttpResponseRedirect(reverse('admin:products_product_changelist'))
+
+        categories = list(original.categories.all())
+        tags = list(original.tags.all())
+        related = list(original.related_products.all())
+
+        original.pk = None
+        original.id = None
+        original.name = f'Copy of {original.name}'
+        original.slug = ''
+        original.is_active = False
+        original.is_featured = False
+        original.review_status = Product.REVIEW_DRAFT
+        original.save()
+
+        original.categories.set(categories)
+        original.tags.set(tags)
+        original.related_products.set(related)
+
+        self.message_user(request, f'Duplicated successfully. Update the name, slug, and Amazon URL before publishing.', messages.SUCCESS)
+        return HttpResponseRedirect(reverse('admin:products_product_change', args=[original.pk]))
 
     @admin.display(description='Categories')
     def get_categories(self, obj):
@@ -101,6 +129,34 @@ class ProductAdmin(admin.ModelAdmin):
     @admin.action(description='Export selected products to CSV')
     def export_selected_csv(self, request, queryset):
         return write_product_csv(queryset)
+
+    @admin.action(description='Duplicate selected product(s)')
+    def duplicate_products(self, request, queryset):
+        count = 0
+        last_pk = None
+        for product in queryset:
+            categories = list(product.categories.all())
+            tags = list(product.tags.all())
+            related = list(product.related_products.all())
+            product.pk = None
+            product.id = None
+            product.name = f'Copy of {product.name}'
+            product.slug = ''
+            product.is_active = False
+            product.is_featured = False
+            product.review_status = Product.REVIEW_DRAFT
+            product.save()
+            product.categories.set(categories)
+            product.tags.set(tags)
+            product.related_products.set(related)
+            last_pk = product.pk
+            count += 1
+
+        if count == 1:
+            self.message_user(request, 'Product duplicated. Update the name, slug, and Amazon URL before publishing.', messages.SUCCESS)
+            return HttpResponseRedirect(reverse('admin:products_product_change', args=[last_pk]))
+
+        self.message_user(request, f'{count} products duplicated as drafts.', messages.SUCCESS)
 
     def export_template(self, request):
         return write_product_csv(Product.objects.none())
